e8c9f1bb9f1ac81155b61c4248f04b26344092c0
thomas.serafini <thomas.serafini@cb577d7c-3596-11de-a81c-fde7d73ceb89>
2009-05-21 11:08:54 +0000

    Web with screenshot gallery

--- web/gallery.php
+++ web/gallery.php
@@ -1,58 +1,123 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-<html>
-<head>
- <title>TDStool screenshot gallery</title>
- <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr" lang="en-US"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" /><title>Screenshot Gallery</title>
+
+<script type="text/javascript" src="script.js"></script>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
 </head>
-
-<body >
+<body>
 <div class="PageBackgroundSimpleGradient"> </div>
-
 <div class="Main">
- <div class="Sheet">
-  <div class="Sheet-tl"></div>
-  <div class="Sheet-tr"><div></div></div>
-  <div class="Sheet-bl"><div></div></div>
-  <div class="Sheet-br"><div></div></div>
-  <div class="Sheet-tc"><div></div></div>
-  <div class="Sheet-bc"><div></div></div>
-  <div class="Sheet-cl"><div></div></div>
-  <div class="Sheet-cr"><div></div></div>
-  <div class="Sheet-cc"></div>
-  <div class="Sheet-body">
-   <div class="Header">
-    <div class="Header-png"></div>
-    <div class="Header-jpeg"></div>
-    <div class="logo"></div>
-   </div>
-   
-   <div class="contentLayout">
-    <div class="content">
-     <div class="Post">
-      <div class="Post-body">
-       <div class="Post-inner">
-   
-
-
-
-
-<BR/>
-Coming soon...
-<BR/>
-<BR/>
-<a href="index.php"><span><span>Back to Home</span></span></a>
-
-
-       </div>
-      </div>
-     </div>
-    </div>
-   </div>
-  </div>
- </div>
+<div class="Sheet">
+<div class="Sheet-tl"></div>
+<div class="Sheet-tr">
+<div></div>
+</div>
+<div class="Sheet-bl">
+<div></div>
+</div>
+<div class="Sheet-br">
+<div></div>
+</div>
+<div class="Sheet-tc">
+<div></div>
+</div>
+<div class="Sheet-bc">
+<div></div>
+</div>
+<div class="Sheet-cl">
+<div></div>
+</div>
+<div class="Sheet-cr">
+<div></div>
+</div>
+<div class="Sheet-cc"></div>
+<div class="Sheet-body">
+<div class="Header">
+<div class="Header-png"></div>
+<div class="Header-jpeg"></div>
+<div class="logo">
+</div>
 </div>
 
-</body>
-</html>
+<div class="contentLayout">
+<div class="content">
+<div class="Post">
+<div class="Post-body">
+<div class="Post-inner">
+<h2 class="PostHeaderIcon-wrapper"><img src="images/PostHeaderIcon.png" alt="PostHeaderIcon" height="12" width="12" /><span class="PostHeader"> Gallery</span></h2>
 
+<table class="table" width="100%" align = "left">
+<tbody>
+<tr>
+<td valign="top" width="49%">
+<a href="gallery/barriera.png">
+<img class="article" src="gallery/barriera_min.png" alt="screenshot1" title="screenshot gallery" style="float: left;" /> </a>
+</td>
+<td valign="top" width="2%">
+</td>
+<td valign="top" width="49%">
+<a href="gallery/fenditura.png">
+<img class="article" src="gallery/fenditura_min.png" alt="screenshot1" title="screenshot gallery" style="float: left;" /> </a>
+</td>
+</tr>
+<tr>
+<td valign="top" width="49%">
+Wave passing through a potential barrier
+</td>
+<td valign="top" width="2%">
+</td>
+<td valign="top" width="49%">
+Wave through a split in a potential barrier
+</td>
+</tr>
 
+<tr>
+<td valign="top" width="49%">
+<a href="gallery/doppia.png">
+<img class="article" src="gallery/doppia_min.png" alt="screenshot1" title="screenshot gallery" style="float: left;" /> </a>
+</td>
+<td valign="top" width="2%">
+</td>
+<td valign="top" width="49%">
+<a href="gallery/edit.png">
+<img class="article" src="gallery/edit_min.png" alt="screenshot1" title="screenshot gallery" style="float: left;" /> </a>
+</td>
+</tr>
+<tr>
+<td valign="top" width="49%">
+Wave through a double split in a potential barrier
+</td>
+<td valign="top" width="2%">
+</td>
+<td valign="top" width="49%">
+Parameter edit interface
+</td>
+</tr>
+
+</tbody>
+</table>
+
+</div>
+</div>
+</div>
+</div>
+<div class="sidebar1">
+
+<?php include("highlights_box.php"); ?>
+<?php include("newsletter_box.php"); ?>
+<?php include("contact_box.php"); ?>
+
+</div>
+</div>
+</div>
+<div class="cleared"></div>
+
+<?php include("footer_dn.php"); ?>
+
+</div>
+</div>
+<div class="cleared"></div>
+</body></html>
