a500cc5aeb7b39b8bb067fb5d7237696a53fc282
thomas.serafini <thomas.serafini@cb577d7c-3596-11de-a81c-fde7d73ceb89>
2009-06-03 09:28:13 +0000

    o

---
+++ src2D/mod_staticdata_mkl.f90
@@ -0,0 +1,21 @@
+!$$$$$$$$$$$$$$$$$$$$$$$$$$$$  stio3D_qtbm  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+MODULE mod_staticdata
+
+  IMPLICIT  NONE
+  SAVE
+  COMPLEX*16, PARAMETER :: i=(0.,1.)
+
+! physics constants   (in MKS)
+  REAL*8, PARAMETER  :: ELMASS0= 9.1095e-31,                       &
+       &                ELCH= 1.602176e-19,                        &
+       &                HBAR= 1.05459e-34,                         &
+       &                HBAR_eV= HBAR / ELCH,                      &
+       &                BOLTZ= 1.3807e-23,                         &
+       &                PIG=3.14159265,                            &
+       &                EPSILON0= 8.854e-12
+  
+  INTEGER, PARAMETER :: OSV = 0   ! OS Version: 0 = linux, 1 = Windows
+  INTEGER, PARAMETER :: SOLVE_METHOD = 0   ! 0 = Box integration, 1 = Split Step
+
+END MODULE mod_staticdata
