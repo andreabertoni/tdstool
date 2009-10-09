7563c46f83d6a429a23c71cbfcef95747b3ff336
thomas.serafini <thomas.serafini@cb577d7c-3596-11de-a81c-fde7d73ceb89>
2009-10-09 07:54:27 +0000

    Split Step with magnetic field

--- trunk/src2D/split_magnetic.f90
+++ trunk/src2D/split_magnetic.f90
@@ -1,36 +1,40 @@
-MODULE mod_splitstep
+MODULE mod_splitmagnetic
   USE mod_staticdata
   USE mod_sparse_fun
   USE mod_indata
   USE mod_filewrite
-  USE mod_splitmagnetic
 !  USE mod_fftw3params
   Use MKL_DFTI
+  USE mod_solve1dLAPACK
   IMPLICIT NONE
   SAVE
 
 CONTAINS
 
-SUBROUTINE SPLITSTEP_ALGO
+SUBROUTINE SPLITMAGNETIC_ALGO
 
-  INTEGER nx, ny, irow, pt, iter
+  INTEGER pt, iter
   COMPLEX*16 :: alpha, beta, k0, k1
-  COMPLEX*16, ALLOCATABLE :: psik(:)
-  REAL*8 dkx, dky, fftnorm
-  REAL*8, ALLOCATABLE :: kx(:), ky(:)
   INTEGER INFO, file_list_index
   REAL*8 next_write_time
   REAL*8, ALLOCATABLE :: potx(:), poty(:)
-  
-!  INTEGER*8 :: planfw, planbk
+
+  REAL*8, ALLOCATABLE :: xgrid(:), ygrid(:)
+  REAL*8, ALLOCATABLE :: kxgrid(:), kygrid(:)
+  COMPLEX*16, ALLOCATABLE :: psik(:,:)
+  REAL*8, ALLOCATABLE :: phiho(:,:, :)
+  REAL*8, ALLOCATABLE :: phiho5(:,:)
+  REAL*8, ALLOCATABLE :: shiftedxgrid2(:)
+  REAL*8, ALLOCATABLE :: potho(:), potho5(:)
+  REAL*8, ALLOCATABLE :: energiesho(:,:)
+  REAL*8 :: cnormx, cnormy, constho
+  REAL*8 :: dx, dy, ky
+  INTEGER :: nn, nx, ny, nkx, nky, numkx, numky
+  REAL*8 :: scale, omegac, lb2, dkx, dky
+
   type(DFTI_DESCRIPTOR), POINTER :: planfw
-  integer   lengths(2)
-  
-  if (magnetic > 1.0e-5) then
-    call SPLITMAGNETIC_ALGO
-    return
-  end if
-  
+  integer   lengths(1)
+
   call INDATA_COMPUTE(INFO, 1)
   if (INFO == 1) then
     return
@@ -44,35 +48,63 @@ SUBROUTINE SPLITSTEP_ALGO
     call write_2D_grid_bin(write_folder, "grid", xnodes, ynodes, numx, numy, write_downsample_x, write_downsample_y)
   end if
 
+  omegac= ELCH*magnetic/mstar
+  lb2= HBAR/(ELCH*magnetic)
+  numkx= numx
+  numky= numy
+  dx = size_x / numx
+  dy = size_y / numy
+  dkx= 2*PIG/size_x
+  dky= 2*PIG/size_y
+
+  ALLOCATE( psik(0:numx-1, 0:numy-1) )
+  ALLOCATE( phiho(0:numx-1, 0:numkx-1, 0:numky-1) )
+  ALLOCATE( phiho5(0:5*numx-1, 0:numkx-1) )
+  ALLOCATE( shiftedxgrid2(0:5*numx-1) )
+  ALLOCATE( potho(0:numx-1) )
+  ALLOCATE( potho5(0:5*numx-1) )
+  ALLOCATE( energiesho(0:numkx-1, 0:numky-1) )
+  ALLOCATE( xgrid(0:numx-1) )
+  ALLOCATE( ygrid(0:numy-1) )
+  ALLOCATE( kxgrid(0:numkx-1) )
+  ALLOCATE( kygrid(0:numky-1) )
+
+  xgrid= (/ (nn*dx+dx/2., nn= 0,numx-1 ) /)
+  ygrid= (/ (nn*dy+dx/2., nn= 0,numy-1 ) /)
+  kxgrid(0:numkx/2)= (/ (nn*dkx, nn= 0,numkx/2 ) /)
+  kygrid(0:numky/2)= (/ (nn*dky, nn= 0,numky/2 ) /)
+  kxgrid(numkx/2+1:numkx-1)= (/ ( -(numkx-nn)*dkx, nn= numkx/2+1,numkx-1 ) /)
+  kygrid(numky/2+1:numky-1)= (/ ( -(numky-nn)*dky, nn= numky/2+1,numky-1 ) /)
+
+  cnormy= ( 1. / SQRT(REAL(numy,8)) )
+  constho= (HBAR/(2.*mstar)) * HBAR/(dx*dx)
+
+  lengths(1) = numy
+  INFO = DftiCreateDescriptor( planfw, DFTI_DOUBLE, DFTI_COMPLEX, 1, lengths)
+  INFO = DftiCommitDescriptor( planfw )
+
+  DO nky= 0, numky-1
+    ky= kygrid(nky)
+    DO nx= 0, 5*numx-1
+      shiftedxgrid2(nx)= (nx-2*numx)*dx+dx/2 - lb2*ky
+      potho5(nx)= mstar/2. * (omegac * shiftedxgrid2(nx))**2
+    END DO
+    CALL solve1dLAPACK( 5*numx-1, numkx, numkx, constho, potho5, energiesho(:, nky), phiho5 )
+    DO nkx = 0, numkx-1
+      DO nx= 0, numx-1
+        phiho(nx, nkx, nky) = phiho5(nx+2*numx, nkx);
+      END DO
+    END DO
+  END DO
+
+
   alpha = -HBAR*HBAR/(2*mstar)
   beta = 4*PIG * nonlin_as * HBAR*HBAR / mstar
   k0 = -i * 0.5 * dt / HBAR
   k1 =  i * dt * alpha / HBAR
-  dkx = 2*PIG/size_x
-  dky = 2*PIG/size_y
-  fftnorm = 1.0 / REAL(numx*numy)
 
   ALLOCATE (potx(1:numx))
   ALLOCATE (poty(1:numy))
-  ALLOCATE (kx(numx))
-  ALLOCATE (ky(numy))
-  ALLOCATE (psik(numx*numy))
-  kx(1:numx/2) = (/ ((nx-1)*dkx , nx= 1, numx/2) /)
-  kx(numx/2+1:numx) = (/ (-(numx-nx+1)*dkx , nx= numx/2+1, numx) /)
-  kx = kx**2;
-  ky(1:numy/2) = (/ ((ny-1)*dky , ny= 1, numy/2) /)
-  ky(numy/2+1:numy) = (/ (-(numy-ny+1)*dky , ny= numy/2+1, numy) /)
-  ky = ky**2;
-
-!  CALL dfftw_plan_dft_2d( planfw, numx, numy, psi, psik,           &
-!     &                                   FFTW_FORWARD,  FFTW_ESTIMATE )
-!  CALL dfftw_plan_dft_2d( planbk, numx, numy, psik, psi,           &
-!     &                                   FFTW_BACKWARD, FFTW_ESTIMATE )
-
-  lengths(1) = numy
-  lengths(2) = numx
-  INFO = DftiCreateDescriptor( planfw, DFTI_DOUBLE, DFTI_COMPLEX, 2, lengths)
-  INFO = DftiCommitDescriptor( planfw )
 
 ! **** Time steps
   next_write_time = 0.
@@ -99,21 +131,40 @@ SUBROUTINE SPLITSTEP_ALGO
       END DO
     END DO
 
-psik = psi;
     ! ****** Linear step
-    INFO = DftiComputeForward( planfw, psik)
-!    CALL dfftw_execute( planfw )
-    pt = 1;
-    DO nx = 1, numx
-      DO ny = 1, numy
-        psik(pt) = psik(pt) * exp(k1 * (kx(nx) + ky(ny)));
-        pt = pt + 1
+  DO nx = 0, numx-1
+    INFO = DftiComputeForward(planfw, psi(nx*numy+1:(nx+1)*numy))
+  END DO
+
+  DO nky= 0, numky-1
+    DO nkx= 0, numkx-1
+      psik(nkx, nky) = 0.
+      DO nx= 0, numx-1
+        psik(nkx, nky) = psik(nkx, nky) + phiho(nx, nkx, nky)*psi(nx*numy+nky+1) ! psi(nky, nx)
       END DO
     END DO
-    psi = psik * fftnorm;
-    INFO = DftiComputeBackward( planfw, psi)
-!    CALL dfftw_execute( planbk )
-!    psi = psi * fftnorm;
+  END DO
+
+  DO nky= 0, numky-1
+    DO nkx= 0, numkx-1
+      psik(nkx,nky) = psik(nkx,nky) * EXP(-i*dt * energiesho(nkx, nky) / HBAR)
+    END DO
+  END DO
+
+  DO nky = 0, numky-1
+    DO nx = 0, numx-1
+      psi(nx*numy+nky+1) = 0.
+      DO nkx = 0, numkx-1
+        psi(nx*numy+nky+1) = psi(nx*numy+nky+1) + phiho(nx, nkx, nky)*psik(nkx, nky)
+      END DO
+    END DO
+  END DO
+
+  scale = 1.0 / REAL(lengths(1), 8)
+  DO nx = 0, numx-1
+    INFO = DftiComputeBackward(planfw, psi(nx*numy+1:(nx+1)*numy))
+  END DO
+  psi = psi * scale
 
     ! ****** Nonlinear half step
     pt = 1;
@@ -148,11 +199,19 @@ psik = psi;
 !  CALL dfftw_destroy_plan( planbk )
   INFO = DftiFreeDescriptor(planfw)
   
+  DEALLOCATE (psik)
+  DEALLOCATE (phiho)
+  DEALLOCATE (phiho5)
+  DEALLOCATE (shiftedxgrid2)
+  DEALLOCATE (xgrid)
+  DEALLOCATE (ygrid)
+  DEALLOCATE (kxgrid)
+  DEALLOCATE (kygrid)
+  DEALLOCATE (potho)
+  DEALLOCATE (potho5)
+  DEALLOCATE (energiesho)
   DEALLOCATE (poty)
   DEALLOCATE (potx)
-  DEALLOCATE (psik)
-  DEALLOCATE (ky)
-  DEALLOCATE (kx)
   DEALLOCATE (psi)
   DEALLOCATE (pot)
   DEALLOCATE (pot_file_static)
@@ -163,4 +222,3 @@ psik = psi;
 END SUBROUTINE
 
 END MODULE
-
